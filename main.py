89e2a583f4f45287652193935dbe13d1260bbd4c
Ethan <rubriclake2@gmail.com>
2023-08-29 21:42:11 -0500

    Add Currency JSON, Add Comments

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import json
 customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
+# Open CTkinter window.
 app = customtkinter.CTk()
 app.resizable(width=False, height=False)
 app.geometry("400x420")
@@ -22,9 +23,11 @@ def convertCurrency():
     amount = amountEntry.get()
     destination = destinationEntry.get().upper()
     
+    # Open JSON File with currency symbols.
     f = open('currencies.json','r', -1, 'utf-8')
     checkList = json.load(f)
     
+    # Check if inputted currencies exist
     if origin not in checkList or destination not in checkList:
         resultLabel.configure(text="Invalid Currencies")
         return
@@ -46,7 +49,7 @@ def convertCurrency():
     resultLabel.configure(text=amountText)    
     
     
-    
+# Set Main Title   
 titleFont = customtkinter.CTkFont(family="", size=36,)
 titleBox = customtkinter.CTkTextbox(app)
 titleBox = customtkinter.CTkTextbox(app, fg_color="transparent", width=400, height=50, corner_radius=0, activate_scrollbars=False, font=titleFont, padx="40", pady="25")
@@ -54,27 +57,30 @@ titleBox.insert("0.0", "Currency Converter")
 titleBox.configure(state="disabled")
 titleBox.pack()
 
+# Reusable Font Tuple
 entryFont = customtkinter.CTkFont(family="",size=19)
+
+# Origin Currency Field
 originEntry = customtkinter.CTkEntry(app, placeholder_text="Origin Currency (e.g. USD)", height=45, width=300, font=entryFont)
 originEntry.place(relx=0.5, rely=0.20, anchor=customtkinter.N)
 
+# Amount of Origin Currency Field
 amountEntry = customtkinter.CTkEntry(app, placeholder_text="Origin Amount", font=entryFont, height=45, width=300)
 amountEntry.place(relx=0.5, rely=0.40, anchor=customtkinter.CENTER)
 
+# Destination Currency Field
 destinationEntry = customtkinter.CTkEntry(app, placeholder_text="Destination Currency (e.g. CAD)", font=entryFont, height=45, width=300, )
 destinationEntry.place(relx=.5, rely=.60, anchor = customtkinter.S)
 
-
+# Convert Button
 button = customtkinter.CTkButton(master=app, text="Convert", command=convertCurrency, width=290, height=40)
 button.place(relx=0.5, rely=0.75, anchor=customtkinter.S)
 
-#resultEntry = customtkinter.CTkEntry(app, justify=customtkinter.CENTER, font=entryFont, height=35, width=300, state="normal", bg_color='transparent')
-#resultEntry.place(relx=.5, rely=.83, anchor = customtkinter.CENTER)
-#resultEntry.insert(0, "Results")
-
+# This is where the results will appear
 resultLabel = customtkinter.CTkLabel(app, height=35, width=300, bg_color='transparent', justify='center', font=customtkinter.CTkFont('Consolas', 25), text="")
 resultLabel.place(relx=.5, rely=.83, anchor=customtkinter.CENTER)
 
+# Make credit link
 link = customtkinter.CTkLabel(app, text="Made by Ethan Kigotho", cursor="hand2", text_color="grey", fg_color='transparent', bg_color='transparent', justify='center', font=('Consolas', 15, "underline"))
 link.place(relx=.30, rely=.9)
 link.bind("<Button-1>", lambda e: openLink())
